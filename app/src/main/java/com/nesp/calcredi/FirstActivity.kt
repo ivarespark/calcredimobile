7b8247b1f6d9ff93394ccd535c27606840bdf465
Ivan Espinosa <ivarespar@gmail.com>
2022-10-27 00:46:20 -0500

    Splash screen. Tema claro. Desactivar boton copiar.

---
+++ app/src/main/java/com/nesp/calcredi/FirstActivity.kt
@@ -0,0 +1,24 @@
+package com.nesp.calcredi
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.util.AttributeSet
+import android.view.View
+import java.util.*
+
+class FirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+
+       Handler().postDelayed({
+           val intent = Intent(this,MainActivity::class.java)
+           startActivity(intent)
+           finish()
+       },3000)
+    }
+}
+
